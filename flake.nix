d4a4a9393eb1495f8cf6d8f71eebef5d998d03f7
Gabriel Sevecek <1851927+gabriel-sevecek@users.noreply.github.com>
2025-12-13 17:39:59 +0100

    Format flake.nix and add biome as dep

--- flake.nix
+++ flake.nix
@@ -6,26 +6,30 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
+      in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
+            biome
             nodejs_24
             prisma
             prisma-engines
             openssl
             nixfmt-tree
-          nodePackages.typescript
+            nodePackages.typescript
           ];
 
           PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
           PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
           PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
-          PRISMA_FMT_BINARY           = "${pkgs.prisma-engines}/bin/prisma-fmt";
+          PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
         };
       }
     );
