a38861df1803917a67f2898dcf7d4d0a338bced4
Gabriel Sevecek <1851927+gabriel-sevecek@users.noreply.github.com>
2025-12-24 22:43:31 +0100

    Remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { auth } from "~/server/better-auth";
 import { getSession } from "~/server/better-auth/server";
-import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
 	const session = await getSession();
@@ -12,6 +9,5 @@ export default async function Home() {
 		redirect("/sign-in");
 	}
 
-	// If user is logged in, redirect to dashboard
 	redirect("/dashboard");
 }
