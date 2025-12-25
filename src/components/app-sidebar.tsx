608307d9d21c2c04245a9423f13071b660fe05f8
Gabriel Sevecek <1851927+gabriel-sevecek@users.noreply.github.com>
2025-12-25 17:05:23 +0100

    Simplify sidebar content

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -2,15 +2,10 @@
 
 import {
 	AudioWaveform,
-	BookOpen,
-	Bot,
 	Command,
-	Frame,
 	GalleryVerticalEnd,
-	Map as MapIcon,
-	PieChart,
+	LayoutDashboard,
 	Settings2,
-	SquareTerminal,
 } from "lucide-react";
 import type * as React from "react";
 
@@ -29,7 +24,6 @@ import {
 	SidebarRail,
 } from "~/components/ui/sidebar";
 
-// This is sample data.
 const data = {
 	teams: [
 		{
@@ -50,106 +44,15 @@ const data = {
 	],
 	navMain: [
 		{
-			title: "Playground",
-			url: "#",
-			icon: SquareTerminal,
+			title: "Dashboard",
+			url: "/dashboard",
+			icon: LayoutDashboard,
 			isActive: true,
-			items: [
-				{
-					title: "History",
-					url: "#",
-				},
-				{
-					title: "Starred",
-					url: "#",
-				},
-				{
-					title: "Settings",
-					url: "#",
-				},
-			],
-		},
-		{
-			title: "Models",
-			url: "#",
-			icon: Bot,
-			items: [
-				{
-					title: "Genesis",
-					url: "#",
-				},
-				{
-					title: "Explorer",
-					url: "#",
-				},
-				{
-					title: "Quantum",
-					url: "#",
-				},
-			],
-		},
-		{
-			title: "Documentation",
-			url: "#",
-			icon: BookOpen,
-			items: [
-				{
-					title: "Introduction",
-					url: "#",
-				},
-				{
-					title: "Get Started",
-					url: "#",
-				},
-				{
-					title: "Tutorials",
-					url: "#",
-				},
-				{
-					title: "Changelog",
-					url: "#",
-				},
-			],
 		},
 		{
 			title: "Settings",
-			url: "#",
+			url: "/settings",
 			icon: Settings2,
-			items: [
-				{
-					title: "General",
-					url: "#",
-				},
-				{
-					title: "Team",
-					url: "#",
-				},
-				{
-					title: "Billing",
-					url: "#",
-				},
-				{
-					title: "Limits",
-					url: "#",
-				},
-			],
-		},
-	],
-	projects: [
-		{
-			name: "Design Engineering",
-			url: "#",
-			icon: Frame,
-		},
-		{
-			name: "Sales & Marketing",
-			url: "#",
-			icon: PieChart,
-		},
-		{
-			name: "Travel",
-			url: "#",
-			icon: MapIcon,
 		},
 	],
 };
