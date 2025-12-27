f749efec5f204e9c52f6401555d86a8434f5640a
Gabriel Sevecek <1851927+gabriel-sevecek@users.noreply.github.com>
2025-12-27 08:14:54 +0100

    Add tabs to competition page

--- src/app/(dashboard)/competition/[id]/page.tsx
+++ src/app/(dashboard)/competition/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { redirect } from "next/navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import { api } from "~/trpc/server";
 
 export default async function CompetitionPage({
@@ -18,7 +19,19 @@ export default async function CompetitionPage({
 
 	return (
 		<div className="rounded-lg border bg-card p-6">
-			<h1 className="font-bold text-2xl">{competition.name}</h1>
+			<h1 className="mb-6 font-bold text-2xl">{competition.name}</h1>
+			<Tabs defaultValue="Matchday">
+				<TabsList>
+					<TabsTrigger className="cursor-pointer" value="matchday">
+						Matchday
+					</TabsTrigger>
+					<TabsTrigger className="cursor-pointer" value="table">
+						Table
+					</TabsTrigger>
+				</TabsList>
+				<TabsContent value="matchday">Matchday info</TabsContent>
+				<TabsContent value="table">Table info</TabsContent>
+			</Tabs>
 		</div>
 	);
 }
