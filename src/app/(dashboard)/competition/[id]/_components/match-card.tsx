3bb55114ee2cda69a9edff7924e4791c2be80e65
Gabriel Sevecek <1851927+gabriel-sevecek@users.noreply.github.com>
2025-12-27 10:29:45 +0100

    Remove optional chaining for now non-null fields

--- src/app/(dashboard)/competition/[id]/_components/match-card.tsx
+++ src/app/(dashboard)/competition/[id]/_components/match-card.tsx
@@ -1,39 +1,35 @@
 import Image from "next/image";
 
-type MatchCardProps = {
+type MatchRowTeam = {
+	name: string;
+	tla: string;
+	crestUrl: string;
+};
+
+type MatchRowProps = {
 	match: {
 		id: number;
 		date: Date;
-		homeTeam?: {
-			name: string;
-			tla: string;
-			crestUrl: string | null;
-		} | null;
-		awayTeam?: {
-			name: string;
-			tla: string;
-			crestUrl: string | null;
-		} | null;
+		homeTeam: MatchRowTeam;
+		awayTeam: MatchRowTeam;
 	};
 };
 
-export function MatchRow({ match }: MatchCardProps) {
+export function MatchRow({ match }: MatchRowProps) {
 	return (
 		<div
 			className="flex items-center gap-4 rounded-lg border p-4"
 			key={match.id}
 		>
 			<div className="flex items-center gap-2">
-				<span className="font-medium">{match.homeTeam?.tla}</span>
-				{match.homeTeam?.crestUrl && (
-					<Image
-						alt={`${match.homeTeam.name} crest`}
-						className="object-contain"
-						height={24}
-						src={match.homeTeam.crestUrl}
-						width={24}
-					/>
-				)}
+				<span className="font-medium">{match.homeTeam.tla}</span>
+				<Image
+					alt={`${match.homeTeam.name} crest`}
+					className="object-contain"
+					height={24}
+					src={match.homeTeam.crestUrl}
+					width={24}
+				/>
 			</div>
 			<div className="font-mono text-sm">
 				{match.date.toLocaleTimeString([], {
@@ -43,16 +39,14 @@ export function MatchRow({ match }: MatchCardProps) {
 				})}
 			</div>
 			<div className="flex items-center gap-2">
-				{match.awayTeam?.crestUrl && (
-					<Image
-						alt={`${match.awayTeam.name} crest`}
-						className="object-contain"
-						height={24}
-						src={match.awayTeam.crestUrl}
-						width={24}
-					/>
-				)}
-				<span className="font-medium">{match.awayTeam?.tla}</span>
+				<Image
+					alt={`${match.awayTeam.name} crest`}
+					className="object-contain"
+					height={24}
+					src={match.awayTeam.crestUrl}
+					width={24}
+				/>
+				<span className="font-medium">{match.awayTeam.tla}</span>
 			</div>
 		</div>
 	);
