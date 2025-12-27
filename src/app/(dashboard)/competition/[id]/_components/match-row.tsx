2cbfb4bfaa9f5a2838b49811b39efb3f4b788f5c
Gabriel Sevecek <1851927+gabriel-sevecek@users.noreply.github.com>
2025-12-27 11:47:01 +0100

    Display result for finished matches

--- src/app/(dashboard)/competition/[id]/_components/match-row.tsx
+++ src/app/(dashboard)/competition/[id]/_components/match-row.tsx
@@ -1,21 +1,31 @@
 import Image from "next/image";
 
-type MatchRowTeam = {
+type Team = {
 	name: string;
 	tla: string;
 	crestUrl: string;
 };
 
-type MatchRowProps = {
-	match: {
-		id: number;
-		date: Date;
-		homeTeam: MatchRowTeam;
-		awayTeam: MatchRowTeam;
-	};
+type Match = {
+	id: number;
+	date: Date;
+	status: string;
+	homeTeamGoals: number | null;
+	awayTeamGoals: number | null;
+	homeTeam: Team;
+	awayTeam: Team;
 };
 
-function TeamCrest({ team }: { team: MatchRowTeam }) {
+type FinishedMatch = Match & {
+	homeTeamGoals: number;
+	awayTeamGoals: number;
+};
+
+type MatchRowProps = {
+	match: Match;
+};
+
+function TeamCrest({ team }: { team: Team }) {
 	return (
 		<Image
 			alt={`${team.name} crest`}
@@ -27,27 +37,62 @@ function TeamCrest({ team }: { team: MatchRowTeam }) {
 	);
 }
 
+function TeamInfo({ children }: { children: React.ReactNode }) {
+	return (
+		<div className="flex w-[60px] justify-between gap-[2px]">{children}</div>
+	);
+}
+
+function isFinished(match: Match): match is FinishedMatch {
+	return (
+		match.status === "FINISHED" &&
+		match.homeTeamGoals !== null &&
+		match.awayTeamGoals !== null
+	);
+}
+
+function MatchResult({
+	homeTeamGoals,
+	awayTeamGoals,
+}: {
+	homeTeamGoals: number;
+	awayTeamGoals: number;
+}) {
+	return (
+		<div className="rounded bg-muted px-3 py-1">
+			{`${homeTeamGoals} - ${awayTeamGoals}`}
+		</div>
+	);
+}
+
 export function MatchRow({ match }: MatchRowProps) {
 	return (
 		<div
 			className="flex items-center gap-4 rounded-lg border p-4"
 			key={match.id}
 		>
-			<div className="flex items-center gap-2">
+			<TeamInfo>
 				<span className="font-medium">{match.homeTeam.tla}</span>
 				<TeamCrest team={match.homeTeam} />
-			</div>
-			<div className="font-mono text-sm">
-				{match.date.toLocaleTimeString([], {
-					hour: "2-digit",
-					minute: "2-digit",
-					hour12: false,
-				})}
-			</div>
-			<div className="flex items-center gap-2">
+			</TeamInfo>
+			{isFinished(match) ? (
+				<MatchResult
+					awayTeamGoals={match.awayTeamGoals}
+					homeTeamGoals={match.homeTeamGoals}
+				/>
+			) : (
+				<div className="rounded border px-2 py-1 text-sm">
+					{match.date.toLocaleTimeString([], {
+						hour: "2-digit",
+						minute: "2-digit",
+						hour12: false,
+					})}
+				</div>
+			)}
+			<TeamInfo>
 				<TeamCrest team={match.awayTeam} />
 				<span className="font-medium">{match.awayTeam.tla}</span>
-			</div>
+			</TeamInfo>
 		</div>
 	);
 }
